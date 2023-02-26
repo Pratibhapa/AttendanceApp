7d1cf574baf4b519de5eb65a0fb09b1af2a07513
Pratibhapa <pratibhaa4321@gmail.com>
2023-02-26 14:48:00 +0545

    App icon

---
+++ app/src/main/java/com/example/attendanceapp/DbHelper.java
@@ -0,0 +1,27 @@
+package com.example.attendanceapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
+
+import androidx.annotation.Nullable;
+
+public class DbHelper extends SQLiteOpenHelper {
+    private static final int VERSION = 1;
+
+
+    public DbHelper(@Nullable Context context) {
+        super(context, "Attendance.db", null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
